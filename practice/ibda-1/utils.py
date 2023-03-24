49d37305a40094390f9b3fa385b89139d889fe1b
Artyom Golenishchev <48377936+heywannafunk@users.noreply.github.com>
2023-03-24 18:55:54 +0300

    Add files via upload

---
+++ practice/ibda-1/utils.py
@@ -0,0 +1,50 @@
+import numpy as np
+import datetime
+import pandas as pd
+import random
+
+
+def _get_year(amplitude:float=1., bias:float=0.):
+    return amplitude * np.sin(np.linspace(-np.pi/2, 3/2*np.pi, 48*365)) + bias
+
+
+def _get_day():
+    return np.sin(np.linspace(-np.pi/2, 3/2*np.pi, 48))
+
+
+def _get_days(n_days:int=365):
+    day = _get_day()
+    days = np.empty((0,))
+    for d in range(365):
+        days = np.append(days, day)
+    return days
+
+
+def _get_period(n_ticks:int=48*7):
+    return np.sin(np.linspace(0, 2*np.pi, n_ticks))
+
+
+def _get_time(year:int=2049):
+    timeline = []
+    dt = datetime.datetime(year, 1, 1, 0, 0, 0)
+    td = datetime.timedelta(minutes=30)
+    for i in range(48*365):
+        timeline.append(dt)
+        dt = dt + td
+    return timeline
+
+
+def _add_noise(weather_data:pd.DataFrame, deviation:float=0.02, outliers:float=0.1, nans:float=0.1, round:int=2):
+    result = weather_data.copy()
+    #add deviation (except datetime)
+    for key in result.columns[1:]:
+        result.loc[key] = np.round(result[key] * random.uniform(1 - deviation, 1 + deviation), round)
+    #add outliers and nans
+    if outliers or nans:
+        for key in result.columns[1:]:
+            for i in range(len(result[key])):
+                if random.random() < nans: 
+                    result.loc[i, key] = None
+                if random.random() < outliers:
+                    result.loc[i, key] = np.round(result[key][i] * 1_000_000, round)
+    return result
---
+++ practice/ibda-1/weather_generators.py
@@ -0,0 +1,118 @@
+import numpy as np
+import random
+import utils
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def generate_temperature(winter_avg:float, summer_avg:float, round:int=2):
+    '''Генерирует синтетические данные о температуре за год.'''
+    
+    amplitude = (np.abs(winter_avg) + np.abs(summer_avg)) / 2
+    
+    # year carrier
+    year = utils._get_year(amplitude, amplitude + winter_avg)
+    
+     # day carrier
+    whole_year_daily = utils._get_days()
+    
+    # modulation
+    year_temperature = year + whole_year_daily
+    
+    year_temperature = np.round(year_temperature, round)  
+    return year_temperature
+
+
+def generate_lighting(round:int=2):
+    '''Генерирует синтетические данные об освещённости за год.'''
+    
+    # modulator
+    modulator = -np.sin(np.linspace(-np.pi/2, 3/2*np.pi, 48))**2 + 1
+    whole_year_modulator = np.empty((0,))
+    for day in range(365):
+        whole_year_modulator = np.append(whole_year_modulator, modulator)
+        
+    # day carrier
+    whole_year_daily = utils._get_days()
+        
+    # year carrier
+    year = utils._get_year()
+    
+    # year lighting 
+    year_lighting = whole_year_daily + (year * whole_year_modulator)/2
+    
+    year_lighting = np.round(year_lighting, round)  
+    return year_lighting
+
+
+def generate_humidity(temperature_data:np.ndarray, round:int=2):
+    '''Генерирует синтетические данные о влажности за год.'''
+    
+    # Аппроксимация зависимости максимальной абсолютной влажности в г/м^3 от температуры:
+    #     y = np.exp(1.3095+0.0565*x)
+    #     (набор значений, по которым проводилась аппроксимация: https://ru.wikipedia.org/wiki/Влажность) 
+    #
+    # Однако, для простоты функция генерирует относительную влажность, исходя из предположения, 
+    # что её величина имеет нормальное распределение. 
+    
+    year_humidity = np.empty((0,), dtype=float)
+    
+    for t in temperature_data:
+        year_humidity = np.append(year_humidity, random.gauss(mu=0.75, sigma=0.04))
+    
+    year_humidity = np.round(year_humidity, round)  
+    return year_humidity
+
+
+def generate_pressure(temperature_data:np.ndarray, humidity_data:np.ndarray, round:int=2):
+    '''Генерирует синтетические данные об атмосферном давлении за год.'''
+    
+    # normalized temperature
+    normalized_temperature = (temperature_data - temperature_data.min()) / (temperature_data.max() - temperature_data.min())
+    normalized_humidity = (humidity_data - humidity_data.min()) / (humidity_data.max() - humidity_data.min())
+    
+    # Отмечены колебания атмосферного давления на уровне моря в пределах 641 — 816 мм рт. ст.
+    # https://ru.wikipedia.org/wiki/Атмосферное_давление
+    year_pressure = np.empty((48*365,), dtype=float)
+    
+    for i, t in enumerate(year_pressure):
+        multiplier = normalized_temperature[i] - 0.2 * normalized_humidity[i]
+        # 800 - условный максимум давления
+        # 700 - условный минимум
+        year_pressure[i] = ((780 - 700) * (1-multiplier)) + 700 
+    
+    year_pressure = np.round(year_pressure, round)
+    return year_pressure
+
+
+def generate_weather(winter_avg:float, summer_avg:float, year:int=2049, save:bool=True, filename:str='weather_data.csv', verbose:bool=False, noise:bool=True, deviation:float=0.25, outliers:float=0.01, nans:float=0.01):
+    '''Генерирует данные о погоде за год.'''
+    
+    t = generate_temperature(winter_avg, summer_avg)
+    l = generate_lighting()
+    h = generate_humidity(t)
+    p = generate_pressure(t, h)
+    time = utils._get_time()
+    
+    weather_data = {'datetime': time,
+                    'temperature': t,
+                    'lighting': l,
+                    'humidity': h,
+                    'pressure': p}
+    
+    df = pd.DataFrame(weather_data)
+    if save:
+        assert(filename.endswith('.csv'))
+        df.to_csv(filename, index=False)
+    
+    if noise:
+        df = utils._add_noise(df, deviation=deviation, outliers=outliers, nans=nans)
+    
+    if verbose:
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+        ax1.plot(t)
+        ax2.plot(l)
+        ax3.plot(h)
+        ax4.plot(p)
+        plt.show()
+    
+    return df
